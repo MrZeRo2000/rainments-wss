6e696d09b73978f4f88518aab639c320be713335
MrZeRo2000 <MrZeRo2000@mail.ru>
2019-12-25 22:11:49 +0200

    first commit

---
+++ src/test/java/com/romanpulov/rainmentswss/RainmentsWssApplicationTests.java
@@ -0,0 +1,13 @@
+package com.romanpulov.rainmentswss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RainmentsWssApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
