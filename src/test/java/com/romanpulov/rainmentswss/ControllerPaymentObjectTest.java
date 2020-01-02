52efd52ded01a7bc11ef29701fd7978acc34803c
MrZeRo2000 <MrZeRo2000@mail.ru>
2020-01-02 17:54:02 +0200

    Adjusted tests for JSON contents

--- src/test/java/com/romanpulov/rainmentswss/ControllerPaymentObjectTest.java
+++ src/test/java/com/romanpulov/rainmentswss/ControllerPaymentObjectTest.java
@@ -1,10 +1,10 @@
 package com.romanpulov.rainmentswss;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.web.context.WebApplicationContext;
 
 @SpringBootTest
@@ -18,8 +18,11 @@ public class ControllerPaymentObjectTest extends ControllerMockMvcTest {
     void mainTest() throws Exception {
 
         this.mvc.perform(MockMvcRequestBuilders.get("/paymentobjects")
-                .accept(MediaType.APPLICATION_JSON_VALUE)
-        );
+                .accept(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$").isArray())
+                .andExpect(MockMvcResultMatchers.jsonPath("$", Matchers.hasSize(0)))
+        ;
     }
 
 }
