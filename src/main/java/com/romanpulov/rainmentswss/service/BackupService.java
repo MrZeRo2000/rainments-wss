a0470d9831011db3d4e7dce54403017f5f1a1898
MrZeRo2000 <MrZeRo2000@mail.ru>
2020-03-25 22:37:00 +0200

    created empty class for backup service and added dependency

---
+++ src/main/java/com/romanpulov/rainmentswss/service/BackupService.java
@@ -0,0 +1,7 @@
+package com.romanpulov.rainmentswss.service;
+
+import com.romanpulov.jutilscore.io.ZipFileUtils;
+import com.romanpulov.jutilscore.storage.BackupUtils;
+
+public class BackupService {
+}
