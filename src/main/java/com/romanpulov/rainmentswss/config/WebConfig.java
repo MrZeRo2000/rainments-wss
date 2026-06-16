2ac254276f77f382e13b09d2126b99554e4f5a13
MrZeRo2000 <MrZeRo2020@outlook.com>
2026-06-16 09:46:00 +0300

    Adjusted logging and CORS web configuration, version 0.4.11

--- src/main/java/com/romanpulov/rainmentswss/config/WebConfig.java
+++ src/main/java/com/romanpulov/rainmentswss/config/WebConfig.java
@@ -1,17 +1,19 @@
 package com.romanpulov.rainmentswss.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-/**
- * <a href="https://www.baeldung.com/spring-cors">...</a>
- */
 @Configuration(proxyBeanMethods = false)
+@Profile("int")
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:4200")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowCredentials(true);
     }
 }
